fc554af4f713ddd393b27df99d980da99fe6ffc3
David Clausen <david@poetic.io>
2020-06-09 16:50:03 -0500

    use file system to support html

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import { serve, Response } from "https://deno.land/std@0.56.0/http/server.ts";
-import { aboutpage } from "./content/aboutpage.ts";
-import { homepage } from "./content/homepage.ts";
+import { readFileStrSync } from "https://deno.land/std/fs/mod.ts";
 
 const s = serve({ port: 8000 });
 
 console.log("http://localhost:8000/");
 
-for await (const req of s) {
+for await (const req of s)  {
+  const page = req.url.includes('/about') ? 'about.html' : 'home.html';
 
-  const body = req.url.includes('/about') ? aboutpage : homepage;
+  const body = readFileStrSync(`./content/${page}`, { encoding: "utf8" });
 
   const response: Response = {
     headers: new Headers({
